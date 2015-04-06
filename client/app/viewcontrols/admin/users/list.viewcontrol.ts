4bb6cc17718ed4b9f1b6af19b063bcfbef30bb92
Darion Welch <darion@getplatypi.com>
2015-04-06 14:14:02 -0500

    editing users ui working

--- client/app/viewcontrols/admin/users/list.viewcontrol.ts
+++ client/app/viewcontrols/admin/users/list.viewcontrol.ts
@@ -3,15 +3,20 @@
 import plat = require('platypus');
 import AdminBaseViewControl = require('../base.viewcontrol');
 import UserRepository = require('../../../repositories/user.repository');
+import models = require('../../../models/user.model');
 
 class ListUsersViewControl extends AdminBaseViewControl {
 	title = 'All Users';
  	templateString = require('./list.viewcontrol.html');
  	context = {
- 		users: []
+ 		users: [],
+ 		editableUser: <models.IUser> {}
  	};
+ 	userFlipper: plat.controls.INamedElement<HTMLLIElement, any>;
+ 	shownElement: number = null;
 
- 	constructor(private userRepository: UserRepository) {
+ 	constructor(private userRepository: UserRepository,
+ 		private userFactory: models.UserFactory) {
  		super();
  	}
 
@@ -19,16 +24,55 @@ class ListUsersViewControl extends AdminBaseViewControl {
  		var context = this.context;
  		
  		this.userRepository.all().then((result) => {
- 			console.log(result);
  			context.users = result;
- 		}, (err) => {
- 			// handle error
  		});
+ 	}
+
+ 	showUserDetails(index: number) {
+ 		var context = this.context;
+
+ 		if (index != this.shownElement) {
+ 			this.hideUserDetails(this.shownElement);
+ 		}
+
+ 		context.editableUser = this.userFactory.create(context.users[index]);
+
+ 		this.dom.addClass(this.userFlipper.element.children[index], 'flip');
+ 		this.shownElement = index;
+ 		console.log(context.editableUser);
+ 	}
+
+ 	hideUserDetails(index: number) {
+ 		this.dom.removeClass(this.userFlipper.element.children[index], 'flip');
+ 	}
+
+ 	enableSave(index: number) {
+ 		this.dom.addClass(this.userFlipper.element.children[index], 'value-changed');
+ 	}
+
+ 	updateUser(user: models.IUser) {
+ 		console.log(user);
+ 	}
+
+ 	cancelUpdate() {
+
+ 	}
+
+ 	changeAvatar(index: number, ev) {
+ 		if (ev.target.files && ev.target.files.length === 1) {
+ 			var reader = new FileReader();
+ 			reader.onload = (e) => {
+ 				this.context.editableUser.avatar = (<any>e.target).result;
+ 			}
+ 			reader.readAsDataURL(ev.target.files[0]);
+ 			this.enableSave(index);
+ 		}
  	}
 }
 
 plat.register.viewControl('listusers-vc', ListUsersViewControl, [
-	UserRepository
+	UserRepository,
+	models.UserFactory
 ]);
 
 export = ListUsersViewControl;
